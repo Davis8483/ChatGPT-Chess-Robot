3c8a64ac59becd5c6a2850771e0cd472e7fd1a80
Davis8483 <Davisprints3D@gmail.com>
2023-03-18 20:08:36 -0500

    Arm Overlay
    Added arm overlay to the terminal board output.

--- chess_bot.py
+++ chess_bot.py
@@ -6,142 +6,154 @@ import matrix_tools
 import os
 
 try:
-  import stockfish
+    import stockfish
 except:
-  subprocess.run(["pip", "install", "stockfish"])
-  import stockfish
+    subprocess.run(["pip", "install", "stockfish"])
+    import stockfish
 
 # initialize stockfish chess engine
-sf = stockfish.Stockfish(path=fr"{os.path.dirname(os.path.abspath(__file__))}\stockfish\stockfish-windows-2022-x86-64-avx2.exe")
+sf = stockfish.Stockfish(
+    path=fr"{os.path.dirname(os.path.abspath(__file__))}\stockfish\stockfish-windows-2022-x86-64-avx2.exe")
 
 _loop = False
-target_x = 192.3
+target_x = -192.3
 target_y = 192.3
 target_z = 0
-pos_x = 192.3
+pos_x = -192.3
 pos_y = 192.3
 grabber_state = "closed"
 
 # stops the mainloop
+
+
 def stop():
-  global _loop
-  _loop = False
+    global _loop
+    _loop = False
 
 # returns if the loop controlling the machine is running
+
+
 def get_status(*_):
-  if _loop:
-    return " 🟢 Connected"
-  else:
-    return " 🔴 Disconnected"
+    if _loop:
+        return " 🟢 Connected"
+    else:
+        return " 🔴 Disconnected"
 
 # returns board visuals
+
+
 def get_visuals(*_):
 
-  #TODO: finnish and make sure any angles are correct
+    #TODO: fix the so many stupid bugs in this function
 
-  output = ''
-
-  # load settings file
-  with open('settings.json') as json_file:
-    settings = json.load(json_file)
-
-  joint1, joint2 = _get_servo_angles(pos_x, pos_y, settings["hardware"]["config"]["length-arm-1"], settings["hardware"]["config"]["length-arm-2"])
-
-  # if connected to the chess board display board with arm overlay 
-  if _loop:
-    end_x, end_y = matrix_tools.calculate_end_coordinates(0, 0, joint1, settings["hardware"]["config"]["length-arm-1"])
-
-    updated_matrix = matrix_tools.draw_line(matrix_tools.string2matrix(sf.get_board_visual(), 2), 0, 0, end_x, end_y)
-
-    # compile list matrix into a singular string
-    for row in updated_matrix:
-      for item in row:
-        output += item
-      output += '\n'
-
-  else:
-    output = sf.get_board_visual()
-
-  return output
-
-# used outside of this file to set the position that the mainloop should try to achieve
-def goto_position(x, y, z):
-  global target_x, target_y, target_z, pos_y, pos_x
-  target_x = x
-  target_y = y
-  target_z = z
-  pos_x = x
-  pos_y = y
-
-def set_grabber(state):
-  global grabber_state
-  grabber_state = state
-
-# inverse kinematics junk, source: https://github.com/aakieu/2-dof-planar/blob/master/python/inverse_kinematics.py
-def _get_servo_angles(x, y, a1, a2):
-  
-  # equations for Inverse kinematics
-  r1 = numpy.sqrt(x ** 2 + y ** 2)  # radius equation
-  phi_1 = -numpy.arccos((a2 ** 2 - a1 ** 2 - r1 ** 2)/(-2 * a1 * r1))  # eqauation 1
-  phi_2 = numpy.arctan2(y, -x)  # equation 2
-  phi_3 = -numpy.arccos((r1 ** 2 - a1 ** 2 - a2 ** 2)/(-2 * a1 * a2)) # equation 3
-
-  theta_1 = 180 - numpy.rad2deg(phi_2 - phi_1)
-
-  theta_2 = numpy.rad2deg(phi_3) + 180
-
-  return theta_1, theta_2
-
-current_speed = 0
-def _get_position(current_x, current_y, target_x, target_y, max_speed, acceleration, loop_speed):
-  global current_speed
-
-  dx = target_x - current_x
-  dy = target_y - current_y
-  distance = numpy.sqrt(dx**2 + dy**2)
-
-  if distance <= acceleration:
-    # We're close enough to the target, just move directly to it
-    current_speed = 0
-    return target_x, target_y
-
-  # Calculate the unit vector towards the target
-  ux = dx / distance
-  uy = dy / distance
-
-  # Calculate distance to accelerate/de-accelerate
-  acceleration_distance = (current_speed ** 2 / (2 * acceleration)) / loop_speed
-  
-  # We're not at the maximum speed yet, accelerate
-  if (distance > acceleration_distance) and (current_speed < max_speed):
-    # We're far from the target, accelerate
-    current_speed += acceleration * loop_speed
-  elif distance < acceleration_distance:
-    # We're close to the target, decelerate
-    current_speed -= acceleration * loop_speed
-
-  # Calculate the new position towards the target
-  delta_x = ux * current_speed
-  delta_y = uy * current_speed
-  new_x = current_x + delta_x
-  new_y = current_y + delta_y
-
-  return new_x, new_y
-
-# mainloop
-def main(serial):
-  global _loop, pos_x, pos_y, settings
-  _loop = True
-  while _loop:
     # load settings file
     with open('settings.json') as json_file:
-      settings = json.load(json_file)
-
-    # pos_x, pos_y = _get_position(pos_x, pos_y, target_x, target_y, settings["hardware"]["max-speed"], settings["hardware"]["acceleration"], 0.1)
+        settings = json.load(json_file)
 
     joint1, joint2 = _get_servo_angles(pos_x, pos_y, settings["hardware"]["config"]["length-arm-1"], settings["hardware"]["config"]["length-arm-2"])
 
-    serial.write(f'{{"data": {{"angle-joint1": {joint1}, "angle-joint2": {joint2}, "position-z": {target_z}}}}}\n'.encode())
-    time.sleep(0.1)
+    # if connected to the chess board display board with arm overlay
+    if _loop:
+        
+        board_matrix = matrix_tools.string2matrix(sf.get_board_visual(), 2)
 
-    
+        # draw arm 1
+        a1_end_x, a1_end_y = matrix_tools.calculate_end_coordinates(5, -1, (joint1 - 90), settings["hardware"]["config"]["length-terminal-arm-1"])
+        updated_matrix = matrix_tools.draw_line(board_matrix, 5, -1, a1_end_x, a1_end_y)
+        
+        # draw arm 2
+        a2_end_x, a2_end_y = matrix_tools.calculate_end_coordinates(a1_end_x, a1_end_y, (joint1 + joint2 - 90), settings["hardware"]["config"]["length-terminal-arm-2"])
+        updated_matrix = matrix_tools.draw_line(updated_matrix, a1_end_x, a1_end_y, a2_end_x, a2_end_y)
+
+        return matrix_tools.matrix2string(updated_matrix)
+
+    else:
+        return (sf.get_board_visual())
+
+
+# used outside of this file to set the position that the mainloop should try to achieve
+def goto_position(x, y, z):
+    global target_x, target_y, target_z, pos_y, pos_x
+    target_x = x
+    target_y = y
+    target_z = z
+    pos_x = x
+    pos_y = y
+
+
+def set_grabber(state):
+    global grabber_state
+    grabber_state = state
+
+# inverse kinematics junk, source: https://github.com/aakieu/2-dof-planar/blob/master/python/inverse_kinematics.py
+def _get_servo_angles(x, y, a1, a2):
+
+    # equations for Inverse kinematics
+    r1 = numpy.sqrt(x ** 2 + y ** 2)  # radius equation
+    phi_1 = -numpy.arccos((a2 ** 2 - a1 ** 2 - r1 ** 2) /
+                          (-2 * a1 * r1))  # eqauation 1
+    phi_2 = numpy.arctan2(y, -x)  # equation 2
+    phi_3 = -numpy.arccos((r1 ** 2 - a1 ** 2 - a2 ** 2) /
+                          (-2 * a1 * a2))  # equation 3
+
+    theta_1 = 180 - numpy.rad2deg(phi_2 - phi_1)
+
+    theta_2 = numpy.rad2deg(phi_3) + 180
+
+    return theta_1, theta_2
+
+
+current_speed = 0
+
+
+def _get_position(current_x, current_y, target_x, target_y, max_speed, acceleration, loop_delay):
+    global current_speed
+
+    dx = target_x - current_x
+    dy = target_y - current_y
+    distance = numpy.sqrt(dx**2 + dy**2)
+
+    if distance <= acceleration:
+        # We're close enough to the target, just move directly to it
+        current_speed = 0
+        return target_x, target_y
+
+    # Calculate the unit vector towards the target
+    ux = dx / distance
+    uy = dy / distance
+
+    # Calculate distance to accelerate/de-accelerate
+    acceleration_distance = (current_speed ** 2 / (2 * acceleration)) / loop_delay
+
+    # We're not at the maximum speed yet, accelerate
+    if (distance > acceleration_distance) and (current_speed < max_speed):
+        # We're far from the target, accelerate
+        current_speed += acceleration * loop_delay
+    elif distance < acceleration_distance:
+        # We're close to the target, decelerate
+        current_speed -= acceleration * loop_delay
+
+    # Calculate the new position towards the target
+    delta_x = ux * current_speed
+    delta_y = uy * current_speed
+    new_x = current_x + delta_x
+    new_y = current_y + delta_y
+
+    return new_x, new_y
+
+# mainloop
+def main(serial):
+    global _loop, pos_x, pos_y, settings
+    _loop = True
+    while _loop:
+        # load settings file
+        with open('settings.json') as json_file:
+            settings = json.load(json_file)
+
+        # pos_x, pos_y = _get_position(pos_x, pos_y, target_x, target_y, settings["hardware"]["max-speed"], settings["hardware"]["acceleration"], 0.1)
+
+        joint1, joint2 = _get_servo_angles(
+            pos_x, pos_y, settings["hardware"]["config"]["length-arm-1"], settings["hardware"]["config"]["length-arm-2"])
+
+        serial.write(f'{{"data": {{"angle-joint1": {joint1}, "angle-joint2": {joint2}, "position-z": {target_z}}}}}\n'.encode())
+        time.sleep(0.1)
--- matrix_tools.py
+++ matrix_tools.py
@@ -1,40 +1,38 @@
 import numpy
 
+# draw a line on a pixel matrix, (0, 0) is in the upper right corner 
 def draw_line(matrix, x1, y1, x2, y2):
-    # determine if line is steep
-    is_steep = abs(y2 - y1) > abs(x2 - x1)
-
-    # if line is steep, transpose the matrix
-    if is_steep:
-        matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
-        x1, y1 = y1, x1
-        x2, y2 = y2, x2
-
-    # swap the endpoints if necessary so that we're always drawing left to right
+    # swap the endpoints if necessary so that we're always drawing right to left
     if x1 > x2:
         x1, x2 = x2, x1
         y1, y2 = y2, y1
 
-    # calculate the slope of the line and the y-intercept
-    dx = x2 - x1
+    # translate coordinates to the upper right corner
+    x1 = len(matrix[0]) - x1 + 1
+    x2 = len(matrix[0]) - x2 + 1
+
+    dx = abs(x2 - x1)
     dy = abs(y2 - y1)
-    slope = dy / dx if dx != 0 else 0
-    y_intercept = y1 - slope * x1
+    sx = -1 if x1 > x2 else 1
+    sy = -1 if y1 > y2 else 1
+    err = dx - dy
 
-    # draw the line, applying inverse text markup to the characters in the matrix
-    x = x1
-    y = y1
-    while x <= x2:
-        if 0 <= x < len(matrix[0]) and 0 <= y < len(matrix):
-            matrix[y][x] = "█" * len(matrix[y][x])
-        y = int(round(slope * x + y_intercept))
-        x += 1
+    while x1 != x2 or y1 != y2:
+        # draw the pixel at (x1, y1)
+        if 0 <= x1 < len(matrix[0]) and 0 <= y1 < len(matrix):
+            matrix[y1][x1] = "█" * len(matrix[y1][x1])
 
-    # if line was steep, transpose the matrix back to its original orientation
-    if is_steep:
-        matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+        e2 = 2 * err
+        if e2 > -dy:
+            err -= dy
+            x1 += sx
+        if e2 < dx:
+            err += dx
+            y1 += sy
 
     return matrix
+
+
 
 def calculate_end_coordinates(start_x, start_y, angle, length):
     # Convert the angle from degrees to radians
@@ -48,29 +46,25 @@ def calculate_end_coordinates(start_x, start_y, angle, length):
     end_x = start_x + delta_x
     end_y = start_y + delta_y
     
-    return end_x, end_y
+    return round(end_x), round(end_y)
 
 # takes in a string and outputs a list matrix
 def string2matrix(string, item_size):
-  list_matrix = string.split('\n')
+    # Split the string into rows, and remove any empty rows
+    rows = [row for row in string.split('\n') if row.strip()]
 
-  new_matrix = []
-  for row in list_matrix:
-    new_row = []
+    # Split each row into items of the specified size
+    matrix = [[row[i:i+item_size] for i in range(0, len(row), item_size)] for row in rows]
 
-    item = ''
-    for character_index in range(len(row)):
-      if len(item) < item_size:
-        item += row[character_index]
-        
-      if len(item) >= item_size:
-        new_row.append(item)
-        item = ''
+    return matrix
 
-    # append any aditional items that did not meet the size constrainst
-    new_row.append(item)
-      
-    new_matrix.append(new_row)
+def matrix2string(matrix):
+   
+    output = ''
 
-  return new_matrix
-  
+    for row in matrix:
+        for item in row:
+            output += item
+        output += '\n'
+
+    return output
